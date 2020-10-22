50e2e606d8749cd0d9022b7cf947cdb060047f72
Louis Franz Schaller <louis.schaller@siemens.com>
2020-10-22 12:36:55 +0200

    Sql Datenbankskript neue Version

--- Datenbank/Datenbank.sql
+++ Datenbank/Datenbank.sql
@@ -1,18 +1,21 @@
 mysql -u root
 
-show databases;
-create database Vermietung;
+create database Autovermietung;
 use Vermietung;
 
-create table Marke (MarkenID int AUTO_INCREMENT NOT NULL PRIMARY KEY, Hersteller int);
-create table Wagenart (WagenID int AUTO_INCREMENT NOT NULL PRIMARY KEY, Klasse char(50));
-create table Kraftstoff (StoffID int AUTO_INCREMENT NOT NULL PRIMARY KEY, Stoff char(50));
-create table Modelle (ModellID int AUTO_INCREMENT NOT NULL PRIMARY KEY, Modell char(50));
+create table Autos (AutoID int AUTO_INCREMENT NOT NULL PRIMARY KEY, Modell char(50), Marke char(50), Kraftstoffart char(50), Leistung int, Typ char(50), Sitzplaetze int, Tueren int);
+create table Vermietung (VermietungID int AUTO_INCREMENT NOT NULL PRIMARY KEY, Anfang date, Ende date, AutoID int, KundenID int);
+create table Kunde (KundenID int AUTO_INCREMENT NOT NULL PRIMARY KEY, Username char(50), Password char(50), Inhaltzusammenhang char(50));
 
-show tables;
+INSERT INTO autos (Modell, Marke, Kraftstoffart, Leistung, Typ, Sitzplaetze, Tueren) VALUES ('Vierer', 'Bmw', 'Diesel', '188', 'Coupe', '4', '3');
+INSERT INTO autos (Modell, Marke, Kraftstoffart, Leistung, Typ, Sitzplaetze, Tueren) VALUES ('Dreier', 'Bmw', 'Diesel', '256', 'Kombi', '5', '5');
+INSERT INTO autos (Modell, Marke, Kraftstoffart, Leistung, Typ, Sitzplaetze, Tueren) VALUES ('LaFerrari', 'Ferrari', 'Benzin', '950', 'Sport', '2', '3');
+INSERT INTO autos (Modell, Marke, Kraftstoffart, Leistung, Typ, Sitzplaetze, Tueren) VALUES ('Golf', 'VW', 'Diesel', '150', 'Kompakt', '5', '5');
+INSERT INTO autos (Modell, Marke, Kraftstoffart, Leistung, Typ, Sitzplaetze, Tueren) VALUES ('Cherokee', 'Jeep', 'Diesel', '165', 'Gelaende', '5', '5');
 
-insert into Wagenart (Klasse) VALUES ("Sport"), ("SUV"), ("Gelände"), ("Coupe"), ("Cabrio");
-insert into Marke (Hersteller) VALUES ("BMW"), ("Mercedes"), ("Porsche"), ("Jeep"), ("VW"), ("Ferrari"), ("Audi");
-insert into Kraftstoff (Stoff) VALUES ("Diesel"), ("Benzin"), ("Elektro");
-insert into Modelle (Modell) VALUES ("LaFerrari"), ("Golf"), ("A3Limo"), ("RS3"), ("M8"), ("G-Klasse"), ("918Spyder"), ("Cherokee"), ("Passat"), ("UP!"), ("M4Cabrio"), ("X6");
+INSERT INTO kunde (Username, Password, Inhaltzusammenhang) VALUES ('Fritzblob', 'her45lo23', 'BmwVierer');
+INSERT INTO kunde (Username, Password, Inhaltzusammenhang) VALUES ('Hildediegart', 'her478956', 'VwGolf');
+INSERT INTO kunde (Username, Password, Inhaltzusammenhang) VALUES ('Herbertsick', 'maur12356', 'FerrariLaFerrari');
+INSERT INTO kunde (Username, Password, Inhaltzusammenhang) VALUES ('Wombatwilli', 'brummdidumm', 'BmwDreier');
+
 
