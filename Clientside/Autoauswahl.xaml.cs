dc49a449e51734d2fd213b764c17894db846060c
RR <rene.roesch.99mail.com#>
2020-10-28 12:20:37 +0100

    logik fertig test fehlgewschlagen

--- Clientside/Autoauswahl.xaml.cs
+++ Clientside/Autoauswahl.xaml.cs
@@ -1,10 +1,10 @@
 ﻿using SerializeLib;
 using System;
 using System.Collections.Generic;
-using System.Windows;
 using System.Text.Json;
-using WebsocketLib;
-
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace Clientside
 {
@@ -13,9 +13,8 @@ namespace Clientside
     /// </summary>
     public partial class Autoauswahl : Window
     {
-        private readonly double taeglicherPreis = 300;
         private List<Car> cars;
-        Client client;
+        int id = -1;
 
         public Autoauswahl()
         {
@@ -24,28 +23,57 @@ namespace Clientside
             DateTime start = (DateTime)Application.Current.Properties["start"];
             DateTime end = (DateTime)Application.Current.Properties["end"];
 
+            Client client = (Client)Application.Current.Properties["client"];
             Request req = new Request(start, end);
             string json = JsonSerializer.Serialize(req);
             string tmp = client.Send(json);
-            Response res = JsonSerializer.Deserialize<Response>(tmp);
+            if(tmp == "")
+                MessageBox.Show("Server is currently not availiable");
+            else {
+                Response res = JsonSerializer.Deserialize<Response>(tmp);
 
-            if (res.errorCode == "ok")
-            {
-                cars = res.cars;
-            }
-            else
-            {
-                MessageBox.Show("Please try again");
-            }
+                if (res.errorCode == "ok")
+                {
+                    cars = res.cars;
+                }
+                else
+                {
+                    MessageBox.Show("Please try again");
+                }
 
-            client = (Client)Application.Current.Properties["client"];
-
-            foreach(Car c in cars)
-            {
-                Marke.Items.Add(c.brand);
-                Typ.Items.Add(c.type);
-                Kraftstoff.Items.Add(c.fueltype);
+                for(int id = 0; id < cars.Count; id++)
+                {
+                    Marke.Items.Add(cars[id].brand);
+                    Typ.Items.Add(cars[id].type);
+                    Kraftstoff.Items.Add(cars[id].fueltype);
+                    AddStack(cars[id].id, cars[id].model);
+                }
             }
+        }
+
+        private void AddStack(int id, string text)
+        {
+            Image img = new Image
+            {
+                Source = new BitmapImage(new Uri($"Pictures/{id}.jpg"))
+            };
+            TextBlock tb = new TextBlock
+            {
+                Text = text
+            };
+
+            StackPanel sp = new StackPanel();
+            sp.Children.Add(img);
+            sp.Children.Add(tb);
+
+            Button b = new Button
+            {
+                Name = id.ToString(),
+                Content = sp
+            };
+            b.Click += new RoutedEventHandler(Model_Click);
+
+            Autos.Children.Add(b);
         }
 
         private void Model_Click(object sender, RoutedEventArgs e)
@@ -55,25 +83,12 @@ namespace Clientside
 
         private void Weiter_Click(object sender, RoutedEventArgs e)
         {
-            /*
-            Request req = new Request(start, end); 
-            string json = JsonSerializer.Serialize(req);
-            string tmp = client.Send(json);
-            Response res = JsonSerializer.Deserialize<Response>(tmp);
-            if (res.errorCode == "ok")
+            if(id == -1)
             {
-                Application.Current.Properties["cars"] = res.cars;
-
-                Autoauswahl a = new Autoauswahl();
-                a.Show();
-                this.Hide();
+                MessageBox.Show("Please select a car");
+                return;
             }
-            else
-            {
-                MessageBox.Show("Please try again");
-            }
-            */
-            Bestellung bestellung = new Bestellung(taeglicherPreis);
+            Bestellung bestellung = new Bestellung(cars[id], id);
             bestellung.Show();
             this.Hide();
         }
